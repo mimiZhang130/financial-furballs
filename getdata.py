9a2a41b0cf513bae56fac1f1a6c19809605ffce0
WeiViv <100058931+WeiViv@users.noreply.github.com>
2024-04-06 18:16:17 -0500

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -30,6 +30,7 @@ myobj = {
 
 # posts the object to api 
 response = requests.post(api_url, headers=headers, json=myobj)
+response = requests.put(api_url, headers=headers, data=json.dumps(myobj))
 # grabs a object from api
 response = requests.get(api_url, headers=headers)
 # converts object to json
